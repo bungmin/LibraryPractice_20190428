9ab5f23ce480d976fbf573dfe43dcf82977a03e4
babobungmin <babobungmin@naver.com>
2019-04-28 13:33:37 +0900

    111111

--- app/src/main/java/com/tj/librarypractice_20190428/MainActivity.java
+++ app/src/main/java/com/tj/librarypractice_20190428/MainActivity.java
@@ -1,19 +1,28 @@
 package com.tj.librarypractice_20190428;
 
+import android.databinding.DataBindingUtil;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.tj.librarypractice_20190428.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
         bindViews();
         setupEvents();
         setValues();
 
+
     }
+
 
     @Override
     public void setupEvents() {
@@ -22,6 +31,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        Glide.with(mContext).load("https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2").into(act.pizzaLogoImgView);
 
     }
 
